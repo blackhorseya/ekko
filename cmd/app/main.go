e02d41b0c564c7e3ded38f5114a03f0539ba69b4
Sean Cheng <blackhorseya@gmail.com>
2020-12-05 08:47:03 +0800

    refactor: use logrus in main file

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/blackhorseya/todo-app/internal/utils/validator"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	target := 4
 	nums := []int{1, 2, 3, 4}
 	if validator.ContainInt(nums, target) {
-		fmt.Printf("nums: %v have %d\n", nums, target)
+		logrus.WithFields(logrus.Fields{
+			"nums":   nums,
+			"target": target,
+		}).Info("nums has target value.")
 	}
 }
