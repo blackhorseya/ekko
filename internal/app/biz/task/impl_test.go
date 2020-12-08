446108b4ac127067f247fa9e2842c2e43ac52e1f
Sean Cheng <blackhorseya@gmail.com>
2020-12-08 10:05:08 +0800

    test: implement task biz testing
    - using testify, mock and suite

--- internal/app/biz/task/impl_test.go
+++ internal/app/biz/task/impl_test.go
@@ -1,16 +1,38 @@
 package task
 
 import (
-	"reflect"
+	"fmt"
 	"testing"
 
+	"github.com/blackhorseya/todo-app/internal/app/biz/task/repository/mocks"
 	"github.com/blackhorseya/todo-app/internal/app/entities"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
 )
 
-func Test_impl_Create(t *testing.T) {
-	// todo: 2020-12-06|21:33|doggy|mock it and complete it
-	// repo := new(mocks.TaskRepo)
+type bizTestSuite struct {
+	suite.Suite
+	taskRepo *mocks.TaskRepo
+	taskBiz  Biz
+}
 
+func (s *bizTestSuite) SetupSuite() {
+}
+
+func (s *bizTestSuite) SetupTest() {
+	s.taskRepo = new(mocks.TaskRepo)
+	biz, err := CreateTaskBiz(s.taskRepo)
+	if err != nil {
+		panic(err)
+	}
+	s.taskBiz = biz
+}
+
+func (s *bizTestSuite) TearDownTest() {
+	s.taskRepo.AssertExpectations(s.T())
+}
+
+func (s *bizTestSuite) Test_impl_Create() {
 	type args struct {
 		t *entities.Task
 	}
@@ -18,7 +40,8 @@ func Test_impl_Create(t *testing.T) {
 		name     string
 		args     args
 		wantTask *entities.Task
-		wantErr  bool
+		wantErr  string
+		mockFunc func()
 	}{
 		{
 			name: "missing title then nil true",
@@ -26,20 +49,50 @@ func Test_impl_Create(t *testing.T) {
 				Title: "",
 			}},
 			wantTask: nil,
-			wantErr:  true,
+			wantErr:  "title must be NOT empty",
+			mockFunc: func() {},
+		},
+		{
+			name: "task then task false",
+			args: args{&entities.Task{
+				Title: "123",
+			}},
+			wantTask: &entities.Task{
+				Title: "123",
+			},
+			wantErr: "",
+			mockFunc: func() {
+				s.taskRepo.On("CreateTask", mock.AnythingOfType("*entities.Task")).Return(&entities.Task{
+					Title: "123",
+				}, nil).Once()
+			},
+		},
+		{
+			name: "task then nil true",
+			args: args{&entities.Task{
+				Title: "456",
+			}},
+			wantTask: nil,
+			wantErr:  "test error",
+			mockFunc: func() {
+				s.taskRepo.On("CreateTask", mock.AnythingOfType("*entities.Task")).Return(
+					nil, fmt.Errorf("test error")).Once()
+			},
 		},
 	}
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			i := &impl{}
-			gotTask, err := i.Create(tt.args.t)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Create() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(gotTask, tt.wantTask) {
-				t.Errorf("Create() gotTask = %v, want %v", gotTask, tt.wantTask)
-			}
-		})
+		tt.mockFunc()
+		task, err := s.taskBiz.Create(tt.args.t)
+		if err != nil {
+			s.EqualErrorf(err, tt.wantErr, "Create() error = [%v], wantErr [%v]", err, tt.wantErr)
+		}
+		if task != nil {
+			s.EqualValuesf(tt.wantTask.Title, task.Title, "Create() task = [%v], wantTask = [%v]", task, tt.wantTask)
+		}
+		s.TearDownTest()
 	}
+}
+
+func TestBizCreate(t *testing.T) {
+	suite.Run(t, new(bizTestSuite))
 }
