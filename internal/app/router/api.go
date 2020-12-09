91a3a6094bce4ed2a6a09b304e65f62a418c22e8
Sean Cheng <blackhorseya@gmail.com>
2020-12-09 16:07:11 +0800

    fix: move swagger setting to web

--- internal/app/router/api.go
+++ internal/app/router/api.go
@@ -1,22 +1,13 @@
 package router
 
 import (
-	"fmt"
-
 	// import docs for swagger
 	_ "github.com/blackhorseya/todo-app/internal/app/docs"
 	"github.com/gin-gonic/gin"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 // RegisterAPI register api group route
 func (r *Router) RegisterAPI(app *gin.Engine) {
-	if mode := gin.Mode(); mode == gin.DebugMode {
-		url := ginSwagger.URL(fmt.Sprintf("http://localhost:%d/docs/doc.json", r.C.HTTP.Port))
-		app.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
-	}
-
 	api := app.Group("/api")
 	{
 		api.GET("/readiness", r.HealthAPI.Readiness)
--- internal/app/web.go
+++ internal/app/web.go
@@ -2,17 +2,29 @@ package app
 
 import (
 	"github.com/blackhorseya/todo-app/internal/app/config"
+	"github.com/blackhorseya/todo-app/internal/app/middlewares"
 	"github.com/blackhorseya/todo-app/internal/app/router"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 // NewGinEngine init a engine of Gin
 func NewGinEngine(r router.IRouter, config *config.Config) *gin.Engine {
 	gin.SetMode(config.RunMode)
+
 	app := gin.Default()
+
+	// logger
+	app.Use(middlewares.LoggerMiddleware())
 
 	// register route to Gin engine
 	_ = r.Register(app)
+
+	// swagger
+	if mode := gin.Mode(); mode == gin.DebugMode {
+		app.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	}
 
 	return app
 }
