590ce34bff2d730b00bd2463bad1693e823353f5
Sean Cheng <blackhorseya@gmail.com>
2020-12-10 10:11:10 +0800

    feat: implement readiness
    - check status of mongo connection

--- internal/app/biz/health/impl.go
+++ internal/app/biz/health/impl.go
@@ -1,15 +1,31 @@
 package health
 
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
 type impl struct {
+	MongoClient *mongo.Client
 }
 
-// NewImpl is a constructor of implement business
-func NewImpl() Biz {
-	return &impl{}
+// NewImpl is a constructor of implement business with parameters
+func NewImpl(mongoClient *mongo.Client) Biz {
+	return &impl{MongoClient: mongoClient}
 }
 
 // Readiness to handle application has been ready
 func (i *impl) Readiness() (ok bool, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	err = i.MongoClient.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return false, err
+	}
+
 	return true, nil
 }
 
