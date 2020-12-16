4464b9aa230363d5ed9159658fc9d3a0fcbafa40
Sean Cheng <blackhorseya@gmail.com>
2020-12-16 14:36:24 +0800

    style: reformat and cleanup code

--- web/src/components/app/app.jsx
+++ web/src/components/app/app.jsx
@@ -1,11 +1,11 @@
-import logo from '../../images/logo.svg';
-import './app.css';
+import logo from '../../images/logo.svg'
+import './app.css'
 
-function App() {
+function App () {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,7 +19,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
