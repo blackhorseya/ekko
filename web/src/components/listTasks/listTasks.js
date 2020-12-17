ebe6255c8512bce97eb804c19ae314147bf8bb3e
Sean Cheng <blackhorseya@gmail.com>
2020-12-17 13:41:46 +0800

    [web] feat: added table pagination

--- web/src/components/listTasks/listTasks.js
+++ web/src/components/listTasks/listTasks.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import {taskActions} from '../../_actions';
 import {connect} from 'react-redux';
-import Title from '../_shared/title';
-import {Table} from '@material-ui/core';
+import {Paper, Table, TableContainer} from '@material-ui/core';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import TableBody from '@material-ui/core/TableBody';
 import Checkbox from '@material-ui/core/Checkbox';
+import TableFooter from '@material-ui/core/TableFooter';
+import TablePagination from '@material-ui/core/TablePagination';
 
 class ListTasks extends React.Component {
   componentDidMount() {
     this.props.list();
+
+    this.handleFirstPage = this.handleFirstPage.bind(this);
+    this.handleLastPage = this.handleLastPage.bind(this);
+    this.handleNextPage = this.handleNextPage.bind(this);
+    this.handleBackPage = this.handleBackPage.bind(this);
+  }
+
+  handleFirstPage(e) {
+    console.log('first page');
+  }
+
+  handleLastPage(e) {
+    console.log('last page');
+  }
+
+  handleNextPage(e) {
+    console.log('next page');
+  }
+
+  handleBackPage(e) {
+    console.log('back page');
   }
 
   render() {
     const {tasks} = this.props;
 
     return (
-        <React.Fragment>
-          <Title>Tasks</Title>
-          <Table>
+        <TableContainer component={Paper}>
+          <Table size="small">
             <TableHead>
               <TableRow>
                 <TableCell>ID</TableCell>
@@ -36,12 +57,18 @@ class ListTasks extends React.Component {
                       <Checkbox checked={row.completed}
                                 inputProps={{'aria-label': 'primary checkbox'}}/>
                     </TableCell>
-                    <TableCell>{row.title}</TableCell>
+                    <TableCell component="th"
+                               scope="row">{row.title}</TableCell>
                   </TableRow>
               ))}
             </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination rowsPerPageOptions={[10, 50]}/>
+              </TableRow>
+            </TableFooter>
           </Table>
-        </React.Fragment>
+        </TableContainer>
     );
   }
 }
