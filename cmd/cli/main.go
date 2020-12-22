f1e136794b1e1e8b1569824100e0dc01cfb8d9bc
Sean Cheng <blackhorseya@gmail.com>
2020-12-22 09:07:50 +0800

    feat: added initConfig func

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+
 	"github.com/blackhorseya/todo-app/internal/app/commands"
 	"github.com/blackhorseya/todo-app/internal/pkg/utils/exit"
+	"github.com/spf13/cobra"
 )
+
+var cfgPath = flag.String("c", "configs/app.yaml", "set config file path")
+
+func init() {
+	flag.Parse()
+
+	cobra.OnInitialize(initConfig)
+}
 
 func main() {
 	err := commands.Execute()
 	if err != nil {
 		exit.Er(err)
 	}
+}
+
+func initConfig() {
+	fmt.Println(*cfgPath)
 }
