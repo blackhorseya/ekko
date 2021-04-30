58d27394798a459b30061a15215010f0050044dd
Sean Cheng <blackhorseya@gmail.com>
2021-04-30 14:06:31 +0800

    feat: update todo list view

--- web/src/features/todo/TodoList.js
+++ web/src/features/todo/TodoList.js
@@ -1,11 +1,13 @@
-import {useSelector} from 'react-redux';
-import {selectTodo} from './todoSlice';
+import {useDispatch, useSelector} from 'react-redux';
+import {listTasks, selectTodo} from './todoSlice';
 
 export function TodoList() {
   const todo = useSelector(selectTodo);
+  const dispatch = useDispatch();
 
   return (
       <div>
+        <button onClick={() => dispatch(listTasks())}>list</button>
         <ul>
           {todo.tasks.map((i) => (
               <li key={i.id}>{i.name}</li>
--- web/src/features/todo/todoSlice.js
+++ web/src/features/todo/todoSlice.js
@@ -1,26 +1,47 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
 
 const initialState = {
-  tasks: [
-    {id: 1, name: 'first todo on redux'},
-    {id: 2, name: 'second todo in list'},
-  ],
+  tasks: [],
+  loading: false,
+  error: false,
 };
+
+export const listTasks = createAsyncThunk(
+    'tasks',
+    async (start, end) => {
+      const resp = await fetch(`http://localhost:8080/api/v1/tasks`);
+      return resp.data;
+    },
+);
 
 export const todoSlice = createSlice({
   name: 'todo',
   initialState: initialState,
   reducers: {
+    list: (state, action) => {
+      state.tasks = action.payload;
+      state.loading = true;
+      state.error = false;
+    },
     add: (state, action) => {
       state.tasks.push({
         id: state.tasks[state.tasks.length - 1].id + 1,
         name: action.payload,
       });
+      state.loading = false;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(listTasks.pending, (state) => {
+      state.loading = true;
+    }).addCase(listTasks.fulfilled, (state, action) => {
+      state.tasks.push(action.payload);
+      state.loading = false;
+    });
   },
 });
 
-export const {add} = todoSlice.actions;
+export const {list, add} = todoSlice.actions;
 
 export const selectTodo = (state) => state.todo;
 
