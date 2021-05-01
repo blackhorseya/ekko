adc73a30c1f476d5866fd521671caee539253285
Sean Cheng <blackhorseya@gmail.com>
2021-05-01 21:24:11 +0800

    feat: added transports in infra

---
+++ internal/pkg/infra/transports/http/middlewares/error.go
@@ -0,0 +1,35 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/blackhorseya/house-management/internal/pkg/entity/er"
+	"github.com/gin-gonic/gin"
+)
+
+// ErrorMiddleware serve caller to format api response
+func ErrorMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			if c.Errors.Last() == nil {
+				return
+			}
+
+			err := c.Errors.Last()
+			c.Errors = c.Errors[:0]
+
+			switch err.Err.(type) {
+			case *er.APPError:
+				appError := err.Err.(*er.APPError)
+				c.AbortWithStatusJSON(appError.Status, appError)
+				break
+			default:
+				c.AbortWithStatus(http.StatusInternalServerError)
+				break
+			}
+		}()
+
+		c.Next()
+
+	}
+}
