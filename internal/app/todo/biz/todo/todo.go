5ec67ed417428ba2491e7766cf226177cacc07a4
Sean Cheng <blackhorseya@gmail.com>
2021-05-01 23:28:37 +0800

    feat: declare todo biz interface

---
+++ internal/app/todo/biz/todo/todo.go
@@ -0,0 +1,32 @@
+package todo
+
+import (
+	"github.com/blackhorseya/todo-app/internal/app/todo/biz/todo/repo"
+	"github.com/blackhorseya/todo-app/internal/pkg/base/contextx"
+	"github.com/blackhorseya/todo-app/internal/pkg/entity/todo"
+	"github.com/google/wire"
+)
+
+// IBiz describe todo business service function
+type IBiz interface {
+	// GetByID serve caller to given task's id to get a task
+	GetByID(ctx contextx.Contextx, id string) (task *todo.Task, err error)
+
+	// List serve caller to list all tasks
+	List(ctx contextx.Contextx, start, end int) (tasks []*todo.Task, total int, err error)
+
+	// Create serve caller to create a task
+	Create(ctx contextx.Contextx, title string) (task *todo.Task, err error)
+
+	// UpdateStatus serve caller to update the task's status by id
+	UpdateStatus(ctx contextx.Contextx, id string, status bool) (task *todo.Task, err error)
+
+	// ChangeTitle serve caller to change the task's title by id
+	ChangeTitle(ctx contextx.Contextx, id string, title string) (task *todo.Task, err error)
+
+	// Delete serve caller to given task's id to delete the task
+	Delete(ctx contextx.Contextx, id string) error
+}
+
+// ProviderSet is a provider set for wire
+var ProviderSet = wire.NewSet(NewImpl, repo.ProviderSet)
