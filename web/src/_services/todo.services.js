037cab7f1999c73fb4a06fbf345adae856c20d2d
Sean Cheng <blackhorseya@gmail.com>
2021-05-04 21:14:17 +0800

    [web] feat: added services

---
+++ web/src/_services/todo.services.js
@@ -0,0 +1,24 @@
+function list(start, end) {
+  const opts = {
+    method: 'GET',
+  };
+
+  return fetch(
+      `${process.env.REACT_APP_API_ENDPOINT}/api/v1/tasks？start=${start}&end=${end}`,
+      opts).
+      then(handlerResp);
+}
+
+function handlerResp(response) {
+  return response.text().then(text => {
+    console.log(text);
+    const resp = text && JSON.parse(text);
+
+    if (!response.ok) {
+      const error = (resp && resp.msg) || response.statusText;
+      return Promise.reject(error);
+    }
+
+    return resp.data;
+  });
+}
