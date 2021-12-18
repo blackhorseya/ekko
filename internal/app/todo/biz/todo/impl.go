c8b0d57b26a8eeb890adb2c1696d4eff3d382420
Sean Zheng <blackhorseya@gmail.com>
2021-12-19 01:30:03 +0800

    refactor: get by id task biz

--- internal/app/todo/biz/todo/impl.go
+++ internal/app/todo/biz/todo/impl.go
@@ -7,8 +7,8 @@ import (
 	"github.com/blackhorseya/todo-app/internal/app/todo/biz/todo/repo"
 	"github.com/blackhorseya/todo-app/internal/pkg/base/contextx"
 	"github.com/blackhorseya/todo-app/internal/pkg/entity/er"
+	"github.com/blackhorseya/todo-app/internal/pkg/entity/todo"
 	"github.com/blackhorseya/todo-app/pb"
-	"github.com/bwmarrin/snowflake"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.uber.org/zap"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -17,32 +17,33 @@ import (
 type impl struct {
 	logger *zap.Logger
 	repo   repo.IRepo
-	node   *snowflake.Node
 }
 
 // NewImpl serve caller to create an IBiz
-func NewImpl(logger *zap.Logger, repo repo.IRepo, node *snowflake.Node) IBiz {
+func NewImpl(logger *zap.Logger, repo repo.IRepo) IBiz {
 	return &impl{
 		logger: logger.With(zap.String("type", "TodoBiz")),
 		repo:   repo,
-		node:   node,
 	}
 }
 
-func (i *impl) GetByID(ctx contextx.Contextx, id int64) (task *pb.Task, err error) {
-	ret, err := i.repo.GetByID(ctx, primitive.ObjectID{})
+func (i *impl) GetByID(ctx contextx.Contextx, id primitive.ObjectID) (task *todo.Task, err error) {
+	if id == primitive.NilObjectID {
+		i.logger.Error(er.ErrEmptyID.Error())
+		return nil, er.ErrEmptyID
+	}
+
+	ret, err := i.repo.GetByID(ctx, id)
 	if err != nil {
-		i.logger.Error(er.ErrGetTask.Error(), zap.Error(err), zap.Int64("id", id))
+		i.logger.Error(er.ErrGetTask.Error(), zap.Error(err), zap.String("id", id.Hex()))
 		return nil, er.ErrGetTask
 	}
 	if ret == nil {
-		i.logger.Error(er.ErrTaskNotExists.Error(), zap.Int64("id", id))
+		i.logger.Error(er.ErrTaskNotExists.Error(), zap.String("id", id.Hex()))
 		return nil, er.ErrTaskNotExists
 	}
 
-	// todo: 2021-12-19|00:50|Sean|impl me
-	panic("impl me")
-	// return ret, nil
+	return ret, nil
 }
 
 func (i *impl) List(ctx contextx.Contextx, start, end int) (tasks []*pb.Task, total int, err error) {
@@ -84,7 +85,6 @@ func (i *impl) Create(ctx contextx.Contextx, title string) (task *pb.Task, err e
 	}
 
 	newTask := &pb.Task{
-		Id:        i.node.Generate().Int64(),
 		Title:     title,
 		CreatedAt: timestamppb.New(time.Now()),
 	}
@@ -100,21 +100,21 @@ func (i *impl) Create(ctx contextx.Contextx, title string) (task *pb.Task, err e
 	// return ret, nil
 }
 
-func (i *impl) UpdateStatus(ctx contextx.Contextx, id int64, status bool) (task *pb.Task, err error) {
+func (i *impl) UpdateStatus(ctx contextx.Contextx, id primitive.ObjectID, status bool) (task *pb.Task, err error) {
 	exists, err := i.repo.GetByID(ctx, primitive.ObjectID{})
 	if err != nil {
-		i.logger.Error(er.ErrGetTask.Error(), zap.Error(err), zap.Int64("id", id))
+		i.logger.Error(er.ErrGetTask.Error(), zap.Error(err), zap.String("id", id.Hex()))
 		return nil, er.ErrGetTask
 	}
 	if exists == nil {
-		i.logger.Error(er.ErrTaskNotExists.Error(), zap.Int64("id", id))
+		i.logger.Error(er.ErrTaskNotExists.Error(), zap.String("id", id.Hex()))
 		return nil, er.ErrTaskNotExists
 	}
 
 	exists.Completed = status
 	ret, err := i.repo.Update(ctx, exists)
 	if err != nil {
-		i.logger.Error(er.ErrUpdateStatusTask.Error(), zap.Error(err), zap.Int64("id", id), zap.Bool("status", status))
+		i.logger.Error(er.ErrUpdateStatusTask.Error(), zap.Error(err), zap.String("id", id.Hex()), zap.Bool("status", status))
 		return nil, er.ErrUpdateStatusTask
 	}
 
@@ -124,7 +124,7 @@ func (i *impl) UpdateStatus(ctx contextx.Contextx, id int64, status bool) (task 
 	// return ret, nil
 }
 
-func (i *impl) ChangeTitle(ctx contextx.Contextx, id int64, title string) (task *pb.Task, err error) {
+func (i *impl) ChangeTitle(ctx contextx.Contextx, id primitive.ObjectID, title string) (task *pb.Task, err error) {
 	if len(title) == 0 {
 		i.logger.Error(er.ErrMissingTitle.Error())
 		return nil, er.ErrMissingTitle
@@ -132,18 +132,18 @@ func (i *impl) ChangeTitle(ctx contextx.Contextx, id int64, title string) (task 
 
 	exists, err := i.repo.GetByID(ctx, primitive.ObjectID{})
 	if err != nil {
-		i.logger.Error(er.ErrGetTask.Error(), zap.Error(err), zap.Int64("id", id))
+		i.logger.Error(er.ErrGetTask.Error(), zap.Error(err), zap.String("id", id.Hex()))
 		return nil, er.ErrGetTask
 	}
 	if exists == nil {
-		i.logger.Error(er.ErrTaskNotExists.Error(), zap.Int64("id", id))
+		i.logger.Error(er.ErrTaskNotExists.Error(), zap.String("id", id.Hex()))
 		return nil, er.ErrTaskNotExists
 	}
 
 	exists.Title = title
 	ret, err := i.repo.Update(ctx, exists)
 	if err != nil {
-		i.logger.Error(er.ErrChangeTitleTask.Error(), zap.Error(err), zap.Int64("id", id), zap.String("title", title))
+		i.logger.Error(er.ErrChangeTitleTask.Error(), zap.Error(err), zap.String("id", id.Hex()), zap.String("title", title))
 		return nil, er.ErrChangeTitleTask
 	}
 
@@ -153,10 +153,10 @@ func (i *impl) ChangeTitle(ctx contextx.Contextx, id int64, title string) (task 
 	// return ret, nil
 }
 
-func (i *impl) Delete(ctx contextx.Contextx, id int64) error {
+func (i *impl) Delete(ctx contextx.Contextx, id primitive.ObjectID) error {
 	err := i.repo.Remove(ctx, primitive.ObjectID{})
 	if err != nil {
-		i.logger.Error(er.ErrDeleteTask.Error(), zap.Error(err), zap.Int64("id", id))
+		i.logger.Error(er.ErrDeleteTask.Error(), zap.Error(err), zap.String("id", id.Hex()))
 		return er.ErrDeleteTask
 	}
 
