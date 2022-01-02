0df2c84fff313b1b179557d9cbe349e771ab21a6
Sean Zheng <blackhorseya@gmail.com>
2022-01-03 05:58:54 +0800

    fix: task response
    SIDE-22

--- internal/pkg/models/taskresponse.go
+++ internal/pkg/models/taskresponse.go
@@ -2,15 +2,16 @@ package models
 
 import (
 	"github.com/blackhorseya/todo-app/internal/pkg/entity/todo"
+	"github.com/blackhorseya/todo-app/pb"
 )
 
 // TaskResponse declare presentation layer task response struct
 type TaskResponse struct {
-	ID        string `json:"id"`
-	Title     string `json:"title"`
-	Completed bool   `json:"completed"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	ID        string        `json:"id"`
+	Title     string        `json:"title"`
+	Status    pb.TaskStatus `json:"status"`
+	CreatedAt string        `json:"created_at"`
+	UpdatedAt string        `json:"updated_at"`
 }
 
 // NewTaskResponse serve caller to create a task response from pb.Task
@@ -18,7 +19,7 @@ func NewTaskResponse(t *todo.Task) *TaskResponse {
 	return &TaskResponse{
 		ID:        t.ID.Hex(),
 		Title:     t.Title,
-		Completed: t.Completed,
+		Status:    t.Status,
 		CreatedAt: t.CreatedAt.Time().UTC().Format(rfc3339Mill),
 		UpdatedAt: t.UpdatedAt.Time().UTC().Format(rfc3339Mill),
 	}
