603c581eb0ffc7aa6f06c91c02ce28a98c99921a
Sean Zheng <blackhorseya@gmail.com>
2022-10-04 07:16:11 +0800

    fix: id too large size

--- internal/app/todo/biz/todo/impl.go
+++ internal/app/todo/biz/todo/impl.go
@@ -78,7 +78,7 @@ func (i *impl) Create(ctx contextx.Contextx, title string) (task *ticket.Task, e
 	}
 
 	newTask := &ticket.Task{
-		ID:     uint64(i.node.Generate().Int64()),
+		ID:     uint64(i.node.Generate().Int64() / 1_000_000),
 		Title:  title,
 		Status: pb.TaskStatus_TASK_STATUS_TODO,
 	}
