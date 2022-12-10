78b986ea6d23b73749b2b03f6b3610da697333a5
Sean Zheng <blackhorseya@gmail.com>
2022-12-10 09:04:18 +0800

    feat: impl get by id

--- cmd/restful/todo/api/v1/tasks/get_id.go
+++ cmd/restful/todo/api/v1/tasks/get_id.go
@@ -1,8 +1,18 @@
 package tasks
 
 import (
+	"net/http"
+
+	"github.com/blackhorseya/todo-app/internal/pkg/errorx"
+	"github.com/blackhorseya/todo-app/pkg/contextx"
+	"github.com/blackhorseya/todo-app/pkg/response"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
+
+type getByIDRequest struct {
+	ID int64 `uri:"id"`
+}
 
 // GetByID
 // @Summary Get a task by id
@@ -17,5 +27,25 @@ import (
 // @Failure 500 {object} er.Error
 // @Router /v1/tasks/{id} [get]
 func (i *impl) GetByID(c *gin.Context) {
-	// todo: 2022/12/8|sean|impl me
+	ctx, ok := c.MustGet(string(contextx.KeyCtx)).(contextx.Contextx)
+	if !ok {
+		_ = c.Error(errorx.ErrContextx)
+		return
+	}
+
+	var req getByIDRequest
+	err := c.ShouldBindUri(&req)
+	if err != nil {
+		ctx.Error(errorx.ErrBindID.Error(), zap.Error(err))
+		_ = c.Error(errorx.ErrBindID)
+		return
+	}
+
+	ret, err := i.biz.GetByID(ctx, req.ID)
+	if err != nil {
+		_ = c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, response.OK.WithData(ret))
 }
