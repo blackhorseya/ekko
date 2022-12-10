ba5dee2b02f8198949a4895b4c0992acdfc445c5
Sean Zheng <blackhorseya@gmail.com>
2022-12-10 11:57:55 +0800

    feat: impl update status api

--- cmd/restful/todo/api/v1/tasks/patch_status.go
+++ cmd/restful/todo/api/v1/tasks/patch_status.go
@@ -1,8 +1,24 @@
 package tasks
 
 import (
+	"net/http"
+	"strconv"
+
+	"github.com/blackhorseya/todo-app/internal/pkg/errorx"
+	"github.com/blackhorseya/todo-app/pkg/contextx"
+	"github.com/blackhorseya/todo-app/pkg/entity/domain/task/model"
+	"github.com/blackhorseya/todo-app/pkg/response"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
+
+const (
+	_formStatus = "status"
+)
+
+type patchStatusIDIDRequest struct {
+	ID int64 `uri:"id"`
+}
 
 // UpdateStatus
 // @Summary Update task's status by id
@@ -17,5 +33,39 @@ import (
 // @Failure 500 {object} er.Error
 // @Router /v1/tasks/{id}/status [patch]
 func (i *impl) UpdateStatus(c *gin.Context) {
-	// todo: 2022/12/8|sean|impl me
+	ctx, ok := c.MustGet(string(contextx.KeyCtx)).(contextx.Contextx)
+	if !ok {
+		_ = c.Error(errorx.ErrContextx)
+		return
+	}
+
+	var req patchStatusIDIDRequest
+	err := c.ShouldBindUri(&req)
+	if err != nil {
+		ctx.Error(errorx.ErrInvalidID.Error(), zap.Error(err))
+		_ = c.Error(errorx.ErrInvalidID)
+		return
+	}
+
+	statusVal, err := strconv.Atoi(c.PostForm(_formStatus))
+	if err != nil {
+		ctx.Error(errorx.ErrInvalidStatus.Error(), zap.String(_formStatus, c.PostForm(_formStatus)))
+		_ = c.Error(errorx.ErrInvalidStatus)
+		return
+	}
+
+	_, ok = model.TaskStatus_name[int32(statusVal)]
+	if !ok {
+		ctx.Error(errorx.ErrInvalidStatus.Error(), zap.String(_formStatus, c.PostForm(_formStatus)))
+		_ = c.Error(errorx.ErrInvalidStatus)
+		return
+	}
+
+	ret, err := i.biz.UpdateStatus(ctx, req.ID, model.TaskStatus(statusVal))
+	if err != nil {
+		_ = c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, response.OK.WithData(ret))
 }
