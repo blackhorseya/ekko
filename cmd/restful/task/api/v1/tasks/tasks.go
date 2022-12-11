aee63a9cdaf3c4574b8c760a41907c23c6ca1559
Sean Zheng <blackhorseya@gmail.com>
2022-12-11 08:50:11 +0800

    feat: bind api router

--- cmd/restful/task/api/v1/tasks/tasks.go
+++ cmd/restful/task/api/v1/tasks/tasks.go
@@ -12,5 +12,10 @@ type impl struct {
 func Handle(g *gin.RouterGroup, biz tb.IBiz) {
 	i := &impl{biz: biz}
 
-	g.Group(":id", i.GetByID)
+	g.GET(":id", i.GetByID)
+	g.GET("", i.List)
+	g.POST("", i.Create)
+	g.PATCH(":id/status", i.UpdateStatus)
+	g.PATCH(":id/title", i.ChangeTitle)
+	g.DELETE(":id", i.Delete)
 }
