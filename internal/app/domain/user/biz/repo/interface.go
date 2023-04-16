ebc45f3342aca5b34d3766eae375b26ee21d3123
Sean Zheng <blackhorseya@gmail.com>
2023-04-16 09:07:59 +0800

    feat: add user repo interface

---
+++ internal/app/domain/user/biz/repo/interface.go
@@ -0,0 +1,24 @@
+//go:generate mockgen -destination=./mock_${GOFILE} -package=repo -source=${GOFILE}
+
+package repo
+
+import (
+	"github.com/blackhorseya/ekko/pkg/contextx"
+	um "github.com/blackhorseya/ekko/pkg/entity/domain/user/model"
+	"github.com/google/wire"
+)
+
+// IRepo declare user repo interface
+type IRepo interface {
+	// GetProfileByUsername serve caller to given username to get profile from users table
+	GetProfileByUsername(ctx contextx.Contextx, username string) (info *um.Profile, err error)
+
+	// GetProfileByID serve caller to given id to get profile from users table
+	GetProfileByID(ctx contextx.Contextx, id int64) (info *um.Profile, err error)
+
+	// Register serve caller to given profile to insert into users table
+	Register(ctx contextx.Contextx, who *um.Profile) error
+}
+
+// MariadbSet is a provider set for mariadb implementation
+var MariadbSet = wire.NewSet()
