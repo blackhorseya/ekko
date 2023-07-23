2db54391f161ad2c7bdd04643828136ccfcae1a8
Sean Zheng <blackhorseya@gmail.com>
2023-07-24 07:12:23 +0800

    feat: add middleware for logging, error handling, and context setup
    - Add middleware for logging with Zap and Gin
    - Add middleware for error handling
    - Add middleware for setting up the context
    - Increase the number of lines of context in the stack trace when recovering from a panic
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/restful/app/restful.go
+++ adapter/restful/app/restful.go
@@ -2,10 +2,14 @@ package app
 
 import (
 	"net/http"
+	"time"
 
 	_ "github.com/blackhorseya/ekko/adapter/restful/api/docs" // swagger docs
 	"github.com/blackhorseya/ekko/pkg/adapters"
+	"github.com/blackhorseya/ekko/pkg/contextx"
+	"github.com/blackhorseya/ekko/pkg/er"
 	"github.com/blackhorseya/ekko/pkg/response"
+	ginzap "github.com/gin-contrib/zap"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -19,6 +23,17 @@ type restful struct {
 
 // NewRestful will create a restful adapter
 func NewRestful(logger *zap.Logger, router *gin.Engine) adapters.Restful {
+	router.Use(ginzap.GinzapWithConfig(logger, &ginzap.Config{
+		TimeFormat: time.RFC3339,
+		UTC:        true,
+		SkipPaths:  []string{"/api/healthz"},
+		TraceID:    false,
+		Context:    nil,
+	}))
+	router.Use(ginzap.RecoveryWithZap(logger, true))
+	router.Use(contextx.WithContextx(logger))
+	router.Use(er.HandleError())
+
 	return &restful{
 		logger: logger,
 		router: router,
