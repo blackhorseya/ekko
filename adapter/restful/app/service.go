814bd78608ce1c0c0b0826b3f62bcb7a9624a1ce
Sean Zheng <blackhorseya@gmail.com>
2023-07-24 06:46:01 +0800

    feat: implement restful service with signal handling
    - Add a new file `adapter/restful/app/service.go`
    - Define a struct `Service` with fields `logger` and `httpserver`
    - Implement a method `NewService` to create a new instance of `Service`
    - Implement a method `Start` to start the restful service
    - Implement a method `AwaitSignal` to await a signal and stop the service
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ adapter/restful/app/service.go
@@ -0,0 +1,61 @@
+package app
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/blackhorseya/ekko/pkg/httpx"
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+)
+
+// Service define the restful service
+type Service struct {
+	logger     *zap.Logger
+	httpserver httpx.Server
+}
+
+// NewService will create a restful service
+func NewService(logger *zap.Logger) *Service {
+	return &Service{
+		logger:     logger,
+		httpserver: nil,
+	}
+}
+
+// Start will start the restful service
+func (s *Service) Start() error {
+	s.logger.Info("starting service")
+
+	if s.httpserver != nil {
+		err := s.httpserver.Start()
+		if err != nil {
+			return errors.Wrap(err, "start http server")
+		}
+	}
+
+	return nil
+}
+
+// AwaitSignal will await the signal
+func (s *Service) AwaitSignal() error {
+	c := make(chan os.Signal, 1)
+	signal.Reset(syscall.SIGTERM, syscall.SIGINT)
+	signal.Notify(c, syscall.SIGTERM, syscall.SIGINT)
+
+	if sig := <-c; true {
+		s.logger.Info("receive signal", zap.String("signal", sig.String()))
+
+		if s.httpserver != nil {
+			err := s.httpserver.Stop()
+			if err != nil {
+				s.logger.Warn("stop http server", zap.Error(err))
+			}
+		}
+
+		os.Exit(0)
+	}
+
+	return nil
+}
