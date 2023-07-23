0459f89d45a420705ed6d02c7090f37600802f54
Sean Zheng <blackhorseya@gmail.com>
2023-07-24 07:33:15 +0800

    feat: add tasks router api

---
+++ adapter/restful/app/v1/tasks/patch_status.go
@@ -0,0 +1,71 @@
+package tasks
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/blackhorseya/ekko/internal/pkg/errorx"
+	"github.com/blackhorseya/ekko/pkg/contextx"
+	"github.com/blackhorseya/ekko/pkg/entity/domain/issue/model"
+	"github.com/blackhorseya/ekko/pkg/response"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+const (
+	_formStatus = "status"
+)
+
+type patchStatusIDRequest struct {
+	ID int64 `uri:"id"`
+}
+
+// UpdateStatus
+// @Summary Update issue's status by id
+// @Description Update issue's status by id
+// @Tags Tasks
+// @Accept application/x-www-form-urlencoded
+// @Produce application/json
+// @Param id path int true "ID of issue"
+// @Param status formData integer true "status"
+// @Success 200 {object} response.Response{data=model.Ticket}
+// @Failure 400 {object} er.Error
+// @Failure 500 {object} er.Error
+// @Router /v1/tasks/{id}/status [patch]
+func (i *impl) UpdateStatus(c *gin.Context) {
+	ctx, ok := c.MustGet(string(contextx.KeyCtx)).(contextx.Contextx)
+	if !ok {
+		_ = c.Error(errorx.ErrContextx)
+		return
+	}
+
+	var req patchStatusIDRequest
+	err := c.ShouldBindUri(&req)
+	if err != nil {
+		ctx.Error(errorx.ErrInvalidID.Error(), zap.Error(err))
+		_ = c.Error(errorx.ErrInvalidID)
+		return
+	}
+
+	statusVal, err := strconv.Atoi(c.PostForm(_formStatus))
+	if err != nil {
+		ctx.Error(errorx.ErrInvalidStatus.Error(), zap.String(_formStatus, c.PostForm(_formStatus)))
+		_ = c.Error(errorx.ErrInvalidStatus)
+		return
+	}
+
+	_, ok = model.TicketStatus_name[int32(statusVal)]
+	if !ok {
+		ctx.Error(errorx.ErrInvalidStatus.Error(), zap.String(_formStatus, c.PostForm(_formStatus)))
+		_ = c.Error(errorx.ErrInvalidStatus)
+		return
+	}
+
+	ret, err := i.biz.UpdateStatus(ctx, req.ID, model.TicketStatus(statusVal))
+	if err != nil {
+		_ = c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, response.OK.WithData(ret))
+}
---
+++ adapter/restful/app/v1/tasks/tasks.go
@@ -0,0 +1,20 @@
+package tasks
+
+import (
+	issueB "github.com/blackhorseya/ekko/pkg/entity/domain/issue/biz"
+	"github.com/gin-gonic/gin"
+)
+
+type impl struct {
+	biz issueB.IBiz
+}
+
+func Handle(g *gin.RouterGroup, biz issueB.IBiz) {
+	i := &impl{biz: biz}
+
+	g.GET(":id", i.GetByID)
+	g.GET("", i.List)
+	g.POST("", i.Create)
+	g.PATCH(":id/status", i.UpdateStatus)
+	g.DELETE(":id", i.Delete)
+}
