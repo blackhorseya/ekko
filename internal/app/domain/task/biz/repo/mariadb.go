3bcc4bcab03e6fafc50ac6c39af5956091dd69a5
Sean Zheng <blackhorseya@gmail.com>
2023-07-30 07:58:42 +0800

    feat: implement ticket management functionality for MariaDB
    - Add a new file `mariadb.go` to the `internal/app/domain/task/biz/repo` directory.
    - Implement the `GetTicketByID` function in the `mariadb` struct.
    - Implement the `ListTickets` function in the `mariadb` struct.
    - Implement the `CountTickets` function in the `mariadb` struct.
    - Implement the `CreateTicket` function in the `mariadb` struct.
    - Implement the `UpdateTicket` function in the `mariadb` struct.
    - Implement the `DeleteTicketByID` function in the `mariadb` struct.
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ internal/app/domain/task/biz/repo/mariadb.go
@@ -0,0 +1,46 @@
+package repo
+
+import (
+	"github.com/blackhorseya/ekko/entity/domain/task/model"
+	"github.com/blackhorseya/ekko/pkg/contextx"
+	"github.com/jmoiron/sqlx"
+)
+
+type mariadb struct {
+	rw *sqlx.DB
+}
+
+// NewMariadb will create an object that represent the IRepo interface
+func NewMariadb() IRepo {
+	return &mariadb{}
+}
+
+func (m *mariadb) GetTicketByID(ctx contextx.Contextx, id string) (ticket *model.Ticket, err error) {
+	// todo: 2023/7/30|sean|implement me
+	panic("implement me")
+}
+
+func (m *mariadb) ListTickets(ctx contextx.Contextx, condition ListTicketsCondition) (tickets []*model.Ticket, total int, err error) {
+	// todo: 2023/7/30|sean|implement me
+	panic("implement me")
+}
+
+func (m *mariadb) CountTickets(ctx contextx.Contextx, condition ListTicketsCondition) (total int, err error) {
+	// todo: 2023/7/30|sean|implement me
+	panic("implement me")
+}
+
+func (m *mariadb) CreateTicket(ctx contextx.Contextx, created *model.Ticket) (ticket *model.Ticket, err error) {
+	// todo: 2023/7/30|sean|implement me
+	panic("implement me")
+}
+
+func (m *mariadb) UpdateTicket(ctx contextx.Contextx, updated *model.Ticket) error {
+	// todo: 2023/7/30|sean|implement me
+	panic("implement me")
+}
+
+func (m *mariadb) DeleteTicketByID(ctx contextx.Contextx, id string) error {
+	// todo: 2023/7/30|sean|implement me
+	panic("implement me")
+}
