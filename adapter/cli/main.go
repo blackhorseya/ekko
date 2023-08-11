2fe4035b79a481bb1d59e31d3738e5203a365c63
Sean Zheng <blackhorseya@gmail.com>
2023-08-12 07:35:27 +0800

    chore: refactor wire generation for improved configuration handling
    - Add logging package import
    - Add spew.Dump(config) call
    - Add wire.go file
    - Add wire_gen.go file
    - Add NewConfig function in wire.go
    - Remove wire.Build(config.NewWithPath) call in NewConfig function in wire.go
    - Remove wire.Build(config.NewWithPath) call in NewConfig function in wire_gen.go
    - Remove unused imports in wire_gen.go
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/cli/main.go
+++ adapter/cli/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 var path = flag.String("c", "", "path to config file (default: $HOME/.ekko/config.yaml)")
@@ -17,5 +20,12 @@ func init() {
 }
 
 func main() {
+	config, err := NewConfig(*path)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	spew.Dump(config)
+
 	fmt.Println("I'm a CLI")
 }
