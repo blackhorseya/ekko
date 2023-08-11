4e7162769370408f6e14da1ef1f4219fbd40c642
Sean Zheng <blackhorseya@gmail.com>
2023-08-12 07:31:24 +0800

    feat: add CLI functionality and default config file path
    - Add a command line flag for specifying a config file path
    - Set the default config file path to $HOME/.ekko/config.yaml
    - Print "I'm a CLI" when the program is run
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/cli/main.go
+++ adapter/cli/main.go
@@ -1,8 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
+
+var path = flag.String("c", "", "path to config file (default: $HOME/.ekko/config.yaml)")
+
+func init() {
+	flag.Parse()
+
+	if *path == "" {
+		*path = os.Getenv("HOME") + "/.ekko/config.yaml"
+	}
+}
 
 func main() {
 	fmt.Println("I'm a CLI")
