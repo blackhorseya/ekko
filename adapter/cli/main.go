9a00ff9a09df5fbd5bd06cae1ba1d5f9ba1b3a6e
Sean Zheng <blackhorseya@gmail.com>
2023-08-12 09:50:25 +0800

    refactor: refactor CLI adapter code
    - Remove unused imports and variables from `adapter/cli/main.go`
    - Remove the `NewConfig` function and related imports from `adapter/cli/wire.go`
    - Remove the `NewConfig` function and related imports from `adapter/cli/wire_gen.go`
    - Update the `NewCmd` function in `adapter/cli/wire.go` and `adapter/cli/wire_gen.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/cli/main.go
+++ adapter/cli/main.go
@@ -1,28 +1,11 @@
 package main
 
 import (
-	"flag"
 	"log"
-	"os"
 )
 
-var path = flag.String("c", "", "path to config file (default: $HOME/.ekko/config.yaml)")
-
-func init() {
-	flag.Parse()
-
-	if *path == "" {
-		*path = os.Getenv("HOME") + "/.ekko/config.yaml"
-	}
-}
-
 func main() {
-	config, err := NewConfig(*path)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	cmd, err := NewCmd(config)
+	cmd, err := NewCmd()
 	if err != nil {
 		log.Fatalln(err)
 	}
--- adapter/cli/wire.go
+++ adapter/cli/wire.go
@@ -6,15 +6,10 @@ package main
 
 import (
 	"github.com/blackhorseya/ekko/adapter/cli/app"
-	"github.com/blackhorseya/ekko/internal/pkg/config"
 	"github.com/blackhorseya/ekko/pkg/adapters"
 	"github.com/google/wire"
 )
 
-func NewConfig(path string) (*config.Config, error) {
-	panic(wire.Build(config.NewWithPath))
-}
-
-func NewCmd(config *config.Config) (adapters.CLI, error) {
+func NewCmd() (adapters.CLI, error) {
 	panic(wire.Build(app.NewCmd))
 }
