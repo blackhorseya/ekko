46a7f3c1c60face73de63060de0e89b40133198c
Sean Zheng <blackhorseya@gmail.com>
2023-08-12 10:08:17 +0800

    refactor: refactor import and function signature in main package
    - Remove the unused import of "github.com/spf13/cobra"
    - Change the function signature of "Run" to return an error
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/cli/app/version/version.go
+++ adapter/cli/app/version/version.go
@@ -6,13 +6,30 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// Options is used to define version cmd options
+type Options struct {
+}
+
+// NewOptions is used to create a new Options instance
+func NewOptions() *Options {
+	return &Options{}
+}
+
 // NewVersionCmd is used to create a new version cmd instance
 func NewVersionCmd() *cobra.Command {
+	o := NewOptions()
+
 	return &cobra.Command{
 		Use:   "version",
 		Short: "Print the version number of ekko",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("ekko version: v0.0.1")
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return o.Run()
 		},
 	}
+}
+
+func (o *Options) Run() error {
+	fmt.Println("ekko version 0.0.1")
+
+	return nil
 }
