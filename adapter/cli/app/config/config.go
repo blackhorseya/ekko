4aee10befe20623737819054b662d0e7db6267ce
Sean Zheng <blackhorseya@gmail.com>
2023-08-12 10:11:40 +0800

    feat: refactor command line options handling
    - Add a new struct `Options` for defining config cmd options
    - Add a function `NewOptions` for creating a new instance of `Options`
    - Change the `Run` field of the `show` command to `RunE` and call `o.Show()` instead of printing
    - Add a new method `Show` to `Options` struct, which currently prints "not implement yet"
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/cli/app/config/config.go
+++ adapter/cli/app/config/config.go
@@ -6,8 +6,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// Options is used to define config cmd options
+type Options struct {
+}
+
+// NewOptions is used to create a new Options instance
+func NewOptions() *Options {
+	return &Options{}
+}
+
 // NewConfigCmd is used to create config command
 func NewConfigCmd() *cobra.Command {
+	o := NewOptions()
+
 	cmd := &cobra.Command{
 		Use:   "config",
 		Short: "config is used to manage ekko config",
@@ -16,10 +27,16 @@ func NewConfigCmd() *cobra.Command {
 	cmd.AddCommand(&cobra.Command{
 		Use:   "show",
 		Short: "show is used to show ekko config",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("show config")
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return o.Show()
 		},
 	})
 
 	return cmd
+}
+
+func (o *Options) Show() error {
+	fmt.Println("not implement yet")
+
+	return nil
 }
