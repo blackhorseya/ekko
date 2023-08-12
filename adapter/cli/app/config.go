ffd0597ac9de3b40d2629f281aa473d08669532f
Sean Zheng <blackhorseya@gmail.com>
2023-08-12 09:03:08 +0800

    refactor: refactor "show" command to use json.MarshalIndent for config display
    - Remove unused import of "github.com/davecgh/go-spew/spew"
    - Refactor the "show" command in the newConfigCmd function to use json.MarshalIndent instead of spew.Dump to display the config
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/cli/app/config.go
+++ adapter/cli/app/config.go
@@ -1,8 +1,10 @@
 package app
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/blackhorseya/ekko/internal/pkg/config"
-	"github.com/davecgh/go-spew/spew"
 	"github.com/spf13/cobra"
 )
 
@@ -16,7 +18,10 @@ func newConfigCmd(config *config.Config) *cobra.Command {
 		Use:   "show",
 		Short: "show is used to show ekko config",
 		Run: func(cmd *cobra.Command, args []string) {
-			spew.Dump(config)
+			bytes, err := json.MarshalIndent(config, "", "  ")
+			cobra.CheckErr(err)
+
+			fmt.Println(string(bytes))
 		},
 	})
 
