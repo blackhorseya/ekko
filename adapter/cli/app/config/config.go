3c6c7a351a12a0197d13ec1daf0c8c8e85a55841
Sean Zheng <blackhorseya@gmail.com>
2023-08-12 11:06:44 +0800

    feat: add error handling and config serialization
    - Add `encoding/json` import
    - Add `github.com/blackhorseya/ekko/internal/pkg/config` import
    - Add `github.com/pkg/errors` import
    - Add `cfg, ok := cmd.Context().Value("config").(*config.Config)` line
    - Add `if !ok { return errors.New("failed to get config") }` line
    - Add `bytes, err := json.MarshalIndent(cfg, "", "  ")` line
    - Add `cobra.CheckErr(err)` line
    - Remove `fmt.Println("not implemented yet")` line
    - Remove `fmt.Println(string(bytes))` line
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/cli/app/config/config.go
+++ adapter/cli/app/config/config.go
@@ -1,8 +1,11 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
 
+	"github.com/blackhorseya/ekko/internal/pkg/config"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
@@ -17,7 +20,15 @@ func NewConfigCmd() *cobra.Command {
 		Use:   "show",
 		Short: "show is used to show ekko config",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Println("not implemented yet")
+			cfg, ok := cmd.Context().Value("config").(*config.Config)
+			if !ok {
+				return errors.New("failed to get config")
+			}
+
+			bytes, err := json.MarshalIndent(cfg, "", "  ")
+			cobra.CheckErr(err)
+
+			fmt.Println(string(bytes))
 
 			return nil
 		},
