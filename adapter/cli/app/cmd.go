c96dc996057c37319e47730f0d2cfe0d73793c40
Sean Zheng <blackhorseya@gmail.com>
2023-08-12 08:02:18 +0800

    refactor: refactor command execution and initialization
    - Initialize `rootCmd` in `NewCmd` method
    - Remove panic and implement `Execute` method
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/cli/app/cmd.go
+++ adapter/cli/app/cmd.go
@@ -2,17 +2,23 @@ package app
 
 import (
 	"github.com/blackhorseya/ekko/pkg/adapters"
+	"github.com/spf13/cobra"
 )
 
 type cmd struct {
+	rootCmd *cobra.Command
 }
 
 // NewCmd is used to create a new cmd instance
 func NewCmd() adapters.CLI {
-	return &cmd{}
+	return &cmd{
+		rootCmd: &cobra.Command{
+			Short:        "ekko is a tool for todo list management",
+			SilenceUsage: true,
+		},
+	}
 }
 
 func (c *cmd) Execute() error {
-	// todo: 2023/8/12|sean|impl me
-	panic("implement me")
+	return c.rootCmd.Execute()
 }
