9d76a6d082af603404430c9e68b746b231f591d1
Sean Zheng <blackhorseya@gmail.com>
2023-09-30 07:09:13 +0800

    feat: refactor server initialization and signal handling
    - Add `httpx` package import
    - Add `gin` package import
    - Modify `newRestful` function signature to include `server` and `router` parameters
    - Add `server` and `router` fields to `impl` struct
    - Modify `Start` method to log a message before initializing routing
    - Modify `Start` method to start the server
    - Modify `Start` method to return an error
    - Modify `AwaitSignal` method to use `os/signal` package
    - Modify `AwaitSignal` method to log a message before receiving signal
    - Modify `AwaitSignal` method to stop the server and log a warning if there is an error
    - Modify `AwaitSignal` method to exit the program
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/ekko/cmd/restful/restful.go
+++ adapter/ekko/cmd/restful/restful.go
@@ -1,17 +1,28 @@
 package restful
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/blackhorseya/ekko/pkg/adapters"
+	"github.com/blackhorseya/ekko/pkg/httpx"
+	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 )
 
 type impl struct {
 	logger *zap.Logger
+
+	server httpx.Server
+	router *gin.Engine
 }
 
-func newRestful(logger *zap.Logger) adapters.Restful {
+func newRestful(logger *zap.Logger, server httpx.Server, router *gin.Engine) adapters.Restful {
 	return &impl{
 		logger: logger.With(zap.String("type", "restful")),
+		server: server,
+		router: router,
 	}
 }
 
@@ -21,11 +32,33 @@ func (i *impl) InitRouting() {
 }
 
 func (i *impl) Start() error {
-	// todo: 2023/9/30|sean|impl me
-	panic("implement me")
+	i.logger.Info("start restful server")
+
+	i.InitRouting()
+
+	err := i.server.Start()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (i *impl) AwaitSignal() error {
-	// todo: 2023/9/30|sean|impl me
-	panic("implement me")
+	c := make(chan os.Signal, 1)
+	signal.Reset(syscall.SIGTERM, syscall.SIGINT)
+	signal.Notify(c, syscall.SIGTERM, syscall.SIGINT)
+
+	if sig := <-c; true {
+		i.logger.Info("receive signal", zap.String("signal", sig.String()))
+
+		err := i.server.Stop()
+		if err != nil {
+			i.logger.Warn("stop restful server", zap.Error(err))
+		}
+
+		os.Exit(0)
+	}
+
+	return nil
 }
