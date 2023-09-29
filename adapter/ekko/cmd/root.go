d5ec16c3fc0dfe56ee6bd54e035a5e90d59c98ae
Sean Zheng <blackhorseya@gmail.com>
2023-09-30 06:57:53 +0800

    docs: refactor `ekko` command line tool documentation and logo
    - Update the short description of the `ekko` command line tool
    - Remove the long description and replace it with the `ekko` logo
    - Comment out the `Run` function for now
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/ekko/cmd/root.go
+++ adapter/ekko/cmd/root.go
@@ -13,13 +13,16 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ekko",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "a command line for ekko",
+	Long: `
+███████╗██╗  ██╗██╗  ██╗ ██████╗ 
+██╔════╝██║ ██╔╝██║ ██╔╝██╔═══██╗
+█████╗  █████╔╝ █████╔╝ ██║   ██║
+██╔══╝  ██╔═██╗ ██╔═██╗ ██║   ██║
+███████╗██║  ██╗██║  ██╗╚██████╔╝
+╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ 
+                                 
+`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
