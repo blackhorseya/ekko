98211035982ffa9dd5b7364b1b3c62a4dd6fa693
Sean Zheng <blackhorseya@gmail.com>
2023-09-30 06:59:23 +0800

    feat: add start command to rootCmd
    - Add a new file `adapter/ekko/cmd/start.go`
    - Define the `startCmd` command in the `start.go` file
    - Add a `Run` function to the `startCmd` command
    - Print "start called" when the `startCmd` command is executed
    - Add `startCmd` as a subcommand of the `rootCmd` command
    - Define flags and configuration settings in the `init` function
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ adapter/ekko/cmd/start.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// startCmd represents the start command
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "start a service",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("start called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(startCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
