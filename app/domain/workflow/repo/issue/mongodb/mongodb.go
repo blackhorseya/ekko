44bf33914548077f7c5a01aa1b3965973f5b7304
Sean Zheng <blackhorseya@gmail.com>
2024-03-10 14:23:46 +0800

    feat: implement MongoDB support in Bazel build system
    - Add a new BUILD.bazel file for mongodb
    - Add a new mongodb.go file for mongodb
    - Define a new type `impl` with a `rw` field in the mongodb.go file
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ app/domain/workflow/repo/issue/mongodb/mongodb.go
@@ -0,0 +1,37 @@
+package mongodb
+
+import (
+	"github.com/blackhorseya/ekko/entity/domain/workflow/agg"
+	"github.com/blackhorseya/ekko/entity/domain/workflow/repo"
+	"github.com/blackhorseya/ekko/pkg/contextx"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type impl struct {
+	rw *mongo.Client
+}
+
+// NewIssueRepo is to create a new issue repository.
+func NewIssueRepo(rw *mongo.Client) repo.IIssueRepo {
+	return &impl{rw: rw}
+}
+
+func (i *impl) GetByID(ctx contextx.Contextx, id string) (item *agg.Issue, err error) {
+	// todo: 2024/3/10|sean|implement me
+	panic("implement me")
+}
+
+func (i *impl) Create(ctx contextx.Contextx, item *agg.Issue) (err error) {
+	// todo: 2024/3/10|sean|implement me
+	panic("implement me")
+}
+
+func (i *impl) Update(ctx contextx.Contextx, item *agg.Issue) (err error) {
+	// todo: 2024/3/10|sean|implement me
+	panic("implement me")
+}
+
+func (i *impl) Delete(ctx contextx.Contextx, id string) (err error) {
+	// todo: 2024/3/10|sean|implement me
+	panic("implement me")
+}
