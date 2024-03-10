54aa9a1104a466cfe411da8ce4df92f85825c1e2
Sean Zheng <blackhorseya@gmail.com>
2024-03-10 14:04:09 +0800

    chore: refactor adapter package dependencies
    - Add `//adapter/restful` and `//pkg/cmdx` as dependencies in `adapter/cmd/BUILD.bazel`
    - Add import statements for `github.com/blackhorseya/ekko/adapter/restful` and `github.com/blackhorseya/ekko/pkg/cmdx` in `adapter/cmd/start.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/cmd/start.go
+++ adapter/cmd/start.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"github.com/blackhorseya/ekko/adapter/restful"
+	"github.com/blackhorseya/ekko/pkg/cmdx"
 	"github.com/spf13/cobra"
 )
 
@@ -11,6 +13,8 @@ var startCmd = &cobra.Command{
 }
 
 func init() {
+	startCmd.AddCommand(cmdx.NewServiceCmd("api", "start a restful server", restful.New))
+
 	rootCmd.AddCommand(startCmd)
 
 	// Here you will define your flags and configuration settings.
