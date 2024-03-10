9b88333276f2438c43aa91a9ca4cdd329db7cba5
Sean Zheng <blackhorseya@gmail.com>
2024-03-10 14:03:16 +0800

    feat: refactor implementation and dependencies in server codebase
    - Add dependencies `pkg/configx`, `pkg/contextx`, `pkg/response`, `@org_uber_go_zap//:zap` to `BUILD.bazel`
    - Add imports for `net/http`, `os`, `os/signal`, `syscall` in `impl.go`
    - Remove `TODO implement me` comments in `Start`, `AwaitSignal`, `InitRouting` functions in `impl.go`
    - Add implementation code in `Start`, `AwaitSignal`, `InitRouting` functions in `impl.go`
    - Add `callback` function implementation in `impl.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/restful/impl.go
+++ adapter/restful/impl.go
@@ -1,9 +1,18 @@
 package restful
 
 import (
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/blackhorseya/ekko/pkg/adapterx"
+	"github.com/blackhorseya/ekko/pkg/configx"
+	"github.com/blackhorseya/ekko/pkg/contextx"
+	"github.com/blackhorseya/ekko/pkg/response"
 	"github.com/blackhorseya/ekko/pkg/transports/httpx"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 type impl struct {
@@ -19,21 +28,55 @@ func newService(server *httpx.Server) adapterx.Servicer {
 }
 
 func (i *impl) Start() error {
-	// TODO implement me
-	panic("implement me")
+	ctx := contextx.Background()
+
+	err := i.InitRouting()
+	if err != nil {
+		return err
+	}
+
+	err = i.server.Start(ctx)
+	if err != nil {
+		return err
+	}
+
+	ctx.Info("start restful server", zap.String("addr", configx.C.HTTP.GetAddr()))
+
+	return nil
 }
 
 func (i *impl) AwaitSignal() error {
-	// TODO implement me
-	panic("implement me")
+	c := make(chan os.Signal, 1)
+	signal.Reset(syscall.SIGTERM, syscall.SIGINT)
+	signal.Notify(c, syscall.SIGTERM, syscall.SIGINT)
+
+	if sig := <-c; true {
+		ctx := contextx.Background()
+		ctx.Info("receive signal", zap.String("signal", sig.String()))
+
+		err := i.server.Stop(ctx)
+		if err != nil {
+			ctx.Error("shutdown restful server error", zap.Error(err))
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (i *impl) InitRouting() error {
-	// TODO implement me
-	panic("implement me")
+	api := i.server.Router.Group("/api")
+	{
+		api.POST("/callback", i.callback)
+	}
+
+	return nil
 }
 
 func (i *impl) GetRouter() *gin.Engine {
-	// TODO implement me
-	panic("implement me")
+	return i.server.Router
+}
+
+func (i *impl) callback(c *gin.Context) {
+	c.JSON(http.StatusInternalServerError, response.Err.WithMessage("not implemented"))
 }
