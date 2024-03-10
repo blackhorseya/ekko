c7779af6858a4266e68ff867b017344f0be91ce5
Sean Zheng <blackhorseya@gmail.com>
2024-03-10 13:46:22 +0800

    feat: create Bazel BUILD files and root command for adapter functionality
    - Add a new BUILD.bazel file for the `adapter` directory
    - Add a new BUILD.bazel file for the `adapter/cmd` directory
    - Add a new `root.go` file under `adapter/cmd`
    - Add a new `main.go` file under `adapter`
    - Define a `go_library` and `go_binary` in the new `adapter/BUILD.bazel` file
    - Define a `go_library` in the new `adapter/cmd/BUILD.bazel` file
    - Define the root command and its associated functions in the new `adapter/cmd/root.go` file
    - Define the `main` function in the new `adapter/main.go` file
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ adapter/cmd/root.go
@@ -0,0 +1,61 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/blackhorseya/ekko/pkg/configx"
+	"github.com/blackhorseya/ekko/pkg/logging"
+	"github.com/spf13/cobra"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "ekko",
+	Short: "ekko",
+	Long: `
+███████╗██╗  ██╗██╗  ██╗ ██████╗ 
+██╔════╝██║ ██╔╝██║ ██╔╝██╔═══██╗
+█████╗  █████╔╝ █████╔╝ ██║   ██║
+██╔══╝  ██╔═██╗ ██╔═██╗ ██║   ██║
+███████╗██║  ██╗██║  ██╗╚██████╔╝
+╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ 
+
+`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ekko.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	err := configx.LoadWithPathAndName(cfgFile, "ekko")
+	cobra.CheckErr(err)
+
+	err = logging.InitWithConfig(configx.C.Log)
+	cobra.CheckErr(err)
+}
---
+++ adapter/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/blackhorseya/ekko/adapter/cmd"
+
+func main() {
+	cmd.Execute()
+}
