5ca81d9f9809436c5fc92ff6df79dc1fac4d6ec4
Sean Zheng <blackhorseya@gmail.com>
2024-03-10 14:42:28 +0800

    feat: consolidate MongoDB package with new model structure
    - Add `model.go` to the `srcs` in `BUILD.bazel`
    - Add a new file `model.go` to the `mongodb` package
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ app/domain/workflow/repo/issue/mongodb/model.go
@@ -0,0 +1,16 @@
+package mongodb
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type issue struct {
+	ID        primitive.ObjectID `bson:"_id"`
+	Title     string             `bson:"title"`
+	Completed bool               `bson:"completed"`
+	OwnerID   primitive.ObjectID `bson:"owner_id"`
+	CreatedAt time.Time          `bson:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at"`
+}
