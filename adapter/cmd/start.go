57bb1eccc8d488db057012c91dc3ad0fffff6d43
Sean Zheng <blackhorseya@gmail.com>
2024-03-10 13:54:19 +0800

    chore: update dependencies and add new files in cmd directory
    - Add `info.go` and `start.go` to the `srcs` list in `cmd` go_library in `BUILD.bazel`
    - Add `info.go` file to the `adapter/cmd` directory
    - Add `start.go` file to the `adapter/cmd` directory
    - Add `contextx` and `zap` dependencies to the `cmd` go_library in `BUILD.bazel`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ adapter/cmd/start.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// startCmd represents the start command
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "start a service",
+}
+
+func init() {
+	rootCmd.AddCommand(startCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
