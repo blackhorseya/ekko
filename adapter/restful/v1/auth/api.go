5ee1db4797a803ba23e0a44096ffd2b61d03d564
Sean Zheng <blackhorseya@gmail.com>
2024-03-22 11:31:35 +0800

    chore: update dependencies and add session management functionality
    - Add `@com_github_gin_contrib_sessions//:sessions` as a dependency in `adapter/restful/v1/auth/BUILD.bazel`
    - Add `github.com/gin-contrib/sessions` as an import in `adapter/restful/v1/auth/api.go`
    - Update the dependency `github.com/bytedance/sonic` to version `v1.11.2` in `go.mod`
    - Update the dependency `github.com/go-playground/validator/v10` to version `v10.18.0` in `go.mod`
    - Update the dependencies `github.com/gorilla/context`, `github.com/gorilla/securecookie`, and `github.com/gorilla/sessions` to versions `v1.1.1`, `v1.1.1`, and `v1.2.1` respectively in `go.mod`
    - Update the dependency `github.com/pelletier/go-toml/v2` to version `v2.1.1` in `go.mod`
    - Update the dependency `google.golang.org/protobuf` to version `v1.32.0` in `go.mod`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/restful/v1/auth/api.go
+++ adapter/restful/v1/auth/api.go
@@ -3,8 +3,10 @@ package auth
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"net/http"
 
 	"github.com/blackhorseya/ekko/pkg/authx"
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
@@ -30,8 +32,21 @@ func Handler(g *gin.RouterGroup, authenticator *authx.Authenticator) {
 
 func login(authenticator *authx.Authenticator) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// todo: 2024/3/22|sean|implement me
-		panic("implement me")
+		state, err := generateRandomState()
+		if err != nil {
+			_ = c.Error(err)
+			return
+		}
+
+		session := sessions.Default(c)
+		session.Set("state", state)
+		err = session.Save()
+		if err != nil {
+			_ = c.Error(err)
+			return
+		}
+
+		c.Redirect(http.StatusTemporaryRedirect, authenticator.AuthCodeURL(state))
 	}
 }
 
