e85d80d02f930e0002174aab5b6f4395538c8675
Sean Zheng <blackhorseya@gmail.com>
2024-04-15 12:49:04 +0800

    feat: refactor RESTful API to use `author` instead of `authenticator` in `todos` package
    - Add the `todos` package to the `BUILD.bazel` file in the `adapter/restful/v1` directory
    - Update the `Handler` function in `api.go` to use `author` instead of `authenticator`
    - Create a new `BUILD.bazel` file and `api.go` file in the `adapter/restful/v1/todos` directory
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/restful/v1/api.go
+++ adapter/restful/v1/api.go
@@ -2,11 +2,13 @@ package v1
 
 import (
 	"github.com/blackhorseya/ekko/adapter/restful/v1/auth"
+	"github.com/blackhorseya/ekko/adapter/restful/v1/todos"
 	"github.com/blackhorseya/ekko/pkg/authx"
 	"github.com/gin-gonic/gin"
 )
 
 // Handler is the api handler.
-func Handler(g *gin.RouterGroup, authenticator *authx.Authenticator) {
-	auth.Handler(g.Group("/auth"), authenticator)
+func Handler(g *gin.RouterGroup, author *authx.Authenticator) {
+	auth.Handler(g.Group("/auth"), author)
+	todos.Handle(g.Group("/todos"))
 }
---
+++ adapter/restful/v1/todos/api.go
@@ -0,0 +1,10 @@
+package todos
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// Handle is the api handler.
+func Handle(g *gin.RouterGroup) {
+
+}
