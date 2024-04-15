fc826b756f0ab592558dea4091f93ee0b05fb263
Sean Zheng <blackhorseya@gmail.com>
2024-04-15 13:06:40 +0800

    feat: improve API documentation with swagger annotations
    - Add swagger annotations for the GetList API endpoint
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/restful/v1/todos/get_list.go
+++ adapter/restful/v1/todos/get_list.go
@@ -5,6 +5,15 @@ import (
 )
 
 // GetList is the api to get todo list.
+// @Summary Get todo list.
+// @Description Get todo list.
+// @Tags todos
+// @Accept json
+// @Produce json
+// @Success 200 {object} response.Response{data=[]agg.Issue}
+// @Failure 400,404 {object} response.Response
+// @Failure 500 {object} response.Response
+// @Router /v1/todos [get]
 func (i *impl) GetList(c *gin.Context) {
 	// todo: 2024/4/15|sean|implement me
 	panic("implement me")
