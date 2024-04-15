1821153fd27620b0f5125f3c6385c895de6e1c6f
Sean Zheng <blackhorseya@gmail.com>
2024-04-15 13:04:48 +0800

    feat: improve todos package functionality
    - Add `get_list.go` to the `srcs` in the `todos` go_library
    - Add `github.com/blackhorseya/ekko/entity/domain/workflow/biz` to the `deps` in the `todos` go_library
    - Add a new file `get_list.go` to the `todos` package
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/restful/v1/todos/api.go
+++ adapter/restful/v1/todos/api.go
@@ -1,10 +1,19 @@
 package todos
 
 import (
+	"github.com/blackhorseya/ekko/entity/domain/workflow/biz"
 	"github.com/gin-gonic/gin"
 )
 
+type impl struct {
+	workflow biz.IWorkflowBiz
+}
+
 // Handle is the api handler.
 func Handle(g *gin.RouterGroup) {
+	instance := &impl{
+		workflow: nil,
+	}
 
+	g.GET("", instance.GetList)
 }
---
+++ adapter/restful/v1/todos/get_list.go
@@ -0,0 +1,11 @@
+package todos
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// GetList is the api to get todo list.
+func (i *impl) GetList(c *gin.Context) {
+	// todo: 2024/4/15|sean|implement me
+	panic("implement me")
+}
