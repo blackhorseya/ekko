717944bc59a5d266f5c04c92d619e0133a4a2c89
Sean Zheng <blackhorseya@gmail.com>
2024-06-03 04:21:20 +0800

    feat: consolidate model package structure
    - Add BUILD.bazel file for the model package
    - Add agg.go file with Todo struct in model package
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ entity/domain/todo/model/agg.go
@@ -0,0 +1,13 @@
+package model
+
+import (
+	"time"
+)
+
+// Todo is the aggregate root of the todo domain
+type Todo struct {
+	ID        string    `json:"id,omitempty" bson:"_id"`
+	Title     string    `json:"title,omitempty" bson:"title"`
+	Done      bool      `json:"done,omitempty" bson:"done"`
+	UpdatedAt time.Time `json:"updated_at,omitempty" bson:"updated_at"`
+}
