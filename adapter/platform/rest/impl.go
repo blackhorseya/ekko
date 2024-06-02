9e392e75336f13006edd6d29a5e616f4ad43c425
Sean Zheng <blackhorseya@gmail.com>
2024-06-03 04:50:19 +0800

    feat: update dependencies and add new functionality
    - Add new dependencies to the `BUILD.bazel` file
    - Add `zap` as a new dependency to the `BUILD.bazel` file
    - Add new imports to the `impl.go` file
    - Implement new methods in the `impl` struct in the `impl.go` file
    - Add a new health check endpoint in the `InitRouting` method in the `impl.go` file
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/platform/rest/impl.go
+++ adapter/platform/rest/impl.go
@@ -1,1 +1,108 @@
 package rest
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/blackhorseya/ekko/adapter/platform/wirex"
+	"github.com/blackhorseya/ekko/app/infra/configx"
+	"github.com/blackhorseya/ekko/pkg/adapterx"
+	"github.com/blackhorseya/ekko/pkg/contextx"
+	"github.com/blackhorseya/ekko/pkg/responsex"
+	"github.com/blackhorseya/ekko/pkg/transports/httpx"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"go.uber.org/zap"
+)
+
+type impl struct {
+	injector *wirex.Injector
+	server   *httpx.Server
+}
+
+func newRestful(injector *wirex.Injector, server *httpx.Server) adapterx.Restful {
+	return &impl{
+		injector: injector,
+		server:   server,
+	}
+}
+
+func newService(injector *wirex.Injector, server *httpx.Server) adapterx.Servicer {
+	return newRestful(injector, server)
+}
+
+func (i *impl) Start() error {
+	ctx := contextx.Background()
+
+	err := i.InitRouting()
+	if err != nil {
+		return err
+	}
+
+	err = i.server.Start(ctx)
+	if err != nil {
+		return err
+	}
+
+	ctx.Info("start restful server", zap.String("swagger_url", fmt.Sprintf(
+		"http://%s/api/docs/index.html",
+		strings.ReplaceAll(configx.A.HTTP.GetAddr(), "0.0.0.0", "localhost"),
+	)))
+
+	return nil
+}
+
+func (i *impl) AwaitSignal() error {
+	c := make(chan os.Signal, 1)
+	signal.Reset(syscall.SIGTERM, syscall.SIGINT)
+	signal.Notify(c, syscall.SIGTERM, syscall.SIGINT)
+
+	if sig := <-c; true {
+		ctx := contextx.Background()
+		ctx.Info("receive signal", zap.String("signal", sig.String()))
+
+		err := i.server.Stop(ctx)
+		if err != nil {
+			ctx.Error("shutdown restful server error", zap.Error(err))
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (i *impl) InitRouting() error {
+	router := i.server.Router
+
+	api := router.Group("/api")
+	{
+		api.GET("/docs/*any", ginSwagger.WrapHandler(
+			swaggerFiles.Handler,
+			ginSwagger.InstanceName("platform_restful"),
+		))
+		api.GET("/healthz", i.Healthz)
+	}
+
+	return nil
+}
+
+func (i *impl) GetRouter() *gin.Engine {
+	return i.server.Router
+}
+
+// Healthz is used to check the health of the service.
+// @Summary Check the health of the service.
+// @Description Check the health of the service.
+// @Tags health
+// @Accept json
+// @Produce json
+// @Success 200 {object} responsex.Response
+// @Failure 500 {object} responsex.Response
+// @Router /healthz [get]
+func (i *impl) Healthz(c *gin.Context) {
+	responsex.OK(c, nil)
+}
