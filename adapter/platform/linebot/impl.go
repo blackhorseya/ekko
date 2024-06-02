546bb7f9840d38eef8b3869d89764c2169bf5f13
Sean Zheng <blackhorseya@gmail.com>
2024-06-03 05:36:13 +0800

    feat: refactor Linebot platform configuration naming
    - Add BUILD.bazel file for linebot platform
    - Add impl.go file for linebot platform
    - Change `Linebot` to `PlatformLinebot` in Configuration struct
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ adapter/platform/linebot/impl.go
@@ -0,0 +1,4 @@
+package linebot
+
+type impl struct {
+}
--- app/infra/configx/config.go
+++ app/infra/configx/config.go
@@ -2,6 +2,6 @@ package configx
 
 // Configuration defines the configuration struct.
 type Configuration struct {
-	PlatformRest Application `json:"platform_rest" yaml:"platformRest"`
-	Linebot      Application `json:"linebot" yaml:"linebot"`
+	PlatformRest    Application `json:"platform_rest" yaml:"platformRest"`
+	PlatformLinebot Application `json:"platform_linebot" yaml:"platformLinebot"`
 }
