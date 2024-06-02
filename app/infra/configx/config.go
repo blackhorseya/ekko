3db8cd5adabc8eea26826e2f458fd078e23afc7d
Sean Zheng <blackhorseya@gmail.com>
2024-06-03 05:31:43 +0800

    feat: add Linebot application configuration in Configuration struct
    - Add a new `Linebot` application configuration in the `Configuration` struct.
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/infra/configx/config.go
+++ app/infra/configx/config.go
@@ -3,4 +3,5 @@ package configx
 // Configuration defines the configuration struct.
 type Configuration struct {
 	PlatformRest Application `json:"platform_rest" yaml:"platformRest"`
+	Linebot      Application `json:"linebot" yaml:"linebot"`
 }
