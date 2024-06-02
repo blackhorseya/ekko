911d97661ac8e1d970a7c408abf0f3a17848e119
Sean Zheng <blackhorseya@gmail.com>
2024-06-03 04:30:08 +0800

    test: consolidate MongoDB testing functionality
    - Add mongodb external test file
    - Import necessary packages for mongodb testing
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- app/domain/todo/repo/todo/mongodb_external_test.go
+++ app/domain/todo/repo/todo/mongodb_external_test.go
@@ -1,3 +1,16 @@
 //go:build external
 
 package todo
+
+import (
+	"github.com/blackhorseya/ekko/entity/domain/todo/repo"
+	"github.com/stretchr/testify/suite"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type suiteMongodbExternal struct {
+	suite.Suite
+
+	rw   *mongo.Client
+	repo repo.ITodoRepo
+}
