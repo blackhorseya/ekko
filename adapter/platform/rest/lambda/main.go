386aacb41a5ebe7b1f015e11640edd67f19262b2
Sean Zheng <blackhorseya@gmail.com>
2024-06-03 05:12:53 +0800

    feat: consolidate lambda-related files into separate directory
    - Add a new BUILD.bazel file for the lambda library
    - Add a new main.go file for the lambda handler
    - Define a lambda_lib go library with dependencies
    - Define a lambda go binary that embeds lambda_lib
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ adapter/platform/rest/lambda/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/blackhorseya/ekko/adapter/platform/rest"
+	"github.com/blackhorseya/ekko/app/infra/configx"
+	"github.com/blackhorseya/ekko/pkg/adapterx"
+	"github.com/spf13/viper"
+)
+
+var ginLambda *ginadapter.GinLambdaV2
+
+// Handler is our lambda handler invoked by the `lambda.Start` function call
+func Handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	return ginLambda.ProxyWithContext(ctx, req)
+}
+
+func main() {
+	_, err := configx.LoadApplication(&configx.C.PlatformRest)
+	if err != nil {
+		panic(err)
+	}
+
+	service, err := rest.New(viper.GetViper())
+	if err != nil {
+		panic(err)
+	}
+
+	restful, ok := service.(adapterx.Restful)
+	if !ok {
+		panic("service is not restful")
+	}
+
+	err = restful.InitRouting()
+	if err != nil {
+		panic(err)
+	}
+
+	ginLambda = ginadapter.NewV2(restful.GetRouter())
+
+	lambda.Start(Handler)
+}
