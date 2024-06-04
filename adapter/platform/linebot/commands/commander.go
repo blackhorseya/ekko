9320c219aed1be0880759551e30ee4b7e5b31925
Sean Zheng <blackhorseya@gmail.com>
2024-06-04 11:39:22 +0800

    feat: refactor text command handling and structure
    - Add a new BUILD.bazel file for the linebot commands
    - Add a new commander.go file for text commands
    - Define the TextCommander interface for executing text commands
    - Implement the NewCommands function to create new text commands
    - Implement the PingCommand struct for the ping command
    - Implement the WhoAmICommand struct for the whoami command
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ adapter/platform/linebot/commands/commander.go
@@ -0,0 +1,59 @@
+package commands
+
+import (
+	idM "github.com/blackhorseya/ekko/entity/domain/identity/model"
+	"github.com/blackhorseya/ekko/pkg/contextx"
+	"github.com/line/line-bot-sdk-go/v8/linebot/messaging_api"
+)
+
+// TextCommander is the interface for text command.
+type TextCommander interface {
+	Execute(ctx contextx.Contextx, who *idM.User, text string) ([]messaging_api.MessageInterface, error)
+}
+
+// NewCommands is the function to create new commands.
+func NewCommands() []TextCommander {
+	return []TextCommander{
+		&PingCommand{},
+		&WhoAmICommand{},
+	}
+}
+
+// PingCommand is the struct for ping command.
+type PingCommand struct {
+}
+
+func (cmd *PingCommand) Execute(
+	ctx contextx.Contextx,
+	who *idM.User,
+	text string,
+) ([]messaging_api.MessageInterface, error) {
+	if text == "/ping" {
+		return []messaging_api.MessageInterface{
+			&messaging_api.TextMessage{
+				Text: "pong",
+			},
+		}, nil
+	}
+
+	return nil, nil
+}
+
+type WhoAmICommand struct {
+}
+
+func (cmd *WhoAmICommand) Execute(
+	ctx contextx.Contextx,
+	who *idM.User,
+	text string,
+) ([]messaging_api.MessageInterface, error) {
+	if text == "/whoami" {
+		return []messaging_api.MessageInterface{
+			&messaging_api.TextMessage{
+				Text: who.ID,
+			},
+		}, nil
+	}
+
+	return nil, nil
+}
