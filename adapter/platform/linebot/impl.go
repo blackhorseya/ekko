39db052613abea8f967ec1161d771ef2125cdff9
Sean Zheng <blackhorseya@gmail.com>
2024-06-04 11:29:48 +0800

    feat: refactor server implementation in `impl.go`
    - Add new dependencies to `BUILD.bazel`
    - Import new packages in `impl.go`
    - Implement server start and signal handling in `impl.go`
    - Initialize routing for the server in `impl.go`
    - Add a callback function in `impl.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/platform/linebot/impl.go
+++ adapter/platform/linebot/impl.go
@@ -1,10 +1,17 @@
 package linebot
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/blackhorseya/ekko/adapter/platform/wirex"
 	"github.com/blackhorseya/ekko/pkg/adapterx"
+	"github.com/blackhorseya/ekko/pkg/contextx"
+	"github.com/blackhorseya/ekko/pkg/responsex"
 	"github.com/blackhorseya/ekko/pkg/transports/httpx"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 type impl struct {
@@ -24,21 +31,59 @@ func newService(injector *wirex.Injector, server *httpx.Server) adapterx.Service
 }
 
 func (i *impl) Start() error {
-	// TODO implement me
-	panic("implement me")
+	ctx := contextx.Background()
+
+	err := i.InitRouting()
+	if err != nil {
+		return err
+	}
+
+	err = i.server.Start(ctx)
+	if err != nil {
+		return err
+	}
+
+	ctx.Info("start server", zap.String("address", i.injector.A.HTTP.GetAddr()))
+
+	return nil
 }
 
 func (i *impl) AwaitSignal() error {
-	// TODO implement me
-	panic("implement me")
+	c := make(chan os.Signal, 1)
+	signal.Reset(syscall.SIGTERM, syscall.SIGINT)
+	signal.Notify(c, syscall.SIGTERM, syscall.SIGINT)
+
+	if sig := <-c; true {
+		ctx := contextx.Background()
+		ctx.Info("receive signal", zap.String("signal", sig.String()))
+
+		err := i.server.Stop(ctx)
+		if err != nil {
+			ctx.Error("shutdown restful server error", zap.Error(err))
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (i *impl) InitRouting() error {
-	// TODO implement me
-	panic("implement me")
+	router := i.server.Router
+
+	// api
+	api := router.Group("/api")
+	{
+		api.POST("/callback", i.callback)
+	}
+
+	return nil
 }
 
 func (i *impl) GetRouter() *gin.Engine {
-	// TODO implement me
-	panic("implement me")
+	return i.server.Router
+}
+
+func (i *impl) callback(c *gin.Context) {
+	// todo: 2024/6/4|sean|implement line bot callback
+	responsex.OK(c, nil)
 }
