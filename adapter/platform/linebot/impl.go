af99a1847c159e4ae0ea801e36679c957cf7ea65
Sean Zheng <blackhorseya@gmail.com>
2024-06-04 11:15:58 +0800

    feat: implement linebot functionality and lambda deployment
    - Add dependencies to `BUILD.bazel` for `linebot`
    - Add implementation for `Start`, `AwaitSignal`, `InitRouting`, and `GetRouter` functions in `impl.go`
    - Add a new `BUILD.bazel` and `main.go` file for `linebot/lambda`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- adapter/platform/linebot/impl.go
+++ adapter/platform/linebot/impl.go
@@ -1,4 +1,44 @@
 package linebot
 
+import (
+	"github.com/blackhorseya/ekko/adapter/platform/wirex"
+	"github.com/blackhorseya/ekko/pkg/adapterx"
+	"github.com/blackhorseya/ekko/pkg/transports/httpx"
+	"github.com/gin-gonic/gin"
+)
+
 type impl struct {
+	injector *wirex.Injector
+	server   *httpx.Server
+}
+
+func newRest(injector *wirex.Injector, server *httpx.Server) adapterx.Restful {
+	return &impl{
+		injector: injector,
+		server:   server,
+	}
+}
+
+func newService(injector *wirex.Injector, server *httpx.Server) adapterx.Servicer {
+	return newRest(injector, server)
+}
+
+func (i *impl) Start() error {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (i *impl) AwaitSignal() error {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (i *impl) InitRouting() error {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (i *impl) GetRouter() *gin.Engine {
+	// TODO implement me
+	panic("implement me")
 }
