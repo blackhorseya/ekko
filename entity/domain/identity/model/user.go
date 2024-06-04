a31cd922e62e776f3b55cb1aba35f8db430f535a
Sean Zheng <blackhorseya@gmail.com>
2024-06-04 11:14:13 +0800

    feat: implement context-based user creation functionality
    - Add dependency `pkg/contextx` to `entity/domain/identity/model/BUILD.bazel`
    - Add import statements for `errors` and `github.com/blackhorseya/ekko/pkg/contextx` in `entity/domain/identity/model/user.go`
    - Add factory function `FromContext` to create a User from a context in `entity/domain/identity/model/user.go`
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- entity/domain/identity/model/user.go
+++ entity/domain/identity/model/user.go
@@ -1,9 +1,25 @@
 package model
 
+import (
+	"errors"
+
+	"github.com/blackhorseya/ekko/pkg/contextx"
+)
+
 // User is an entity that represents a user
 type User struct {
 	ID     string `json:"id,omitempty"`
 	Active bool   `json:"active,omitempty"`
 
 	Profile Profile `json:"profile,omitempty"`
+}
+
+// FromContext is a factory function that creates a User from a context.
+func FromContext(ctx contextx.Contextx) (*User, error) {
+	user, ok := ctx.Value(contextx.KeyWho).(*User)
+	if !ok {
+		return nil, errors.New("user not found in context")
+	}
+
+	return user, nil
 }
