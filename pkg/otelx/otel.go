660d1aab4c5231ab6b614d46221e684b411366a4
Sean Zheng <blackhorseya@gmail.com>
2024-06-08 13:07:49 +0800

    feat: consolidate new package `pkg/otelx` changes
    - Add a new `BUILD.bazel` file for the `pkg/otelx` package
    - Add a new `otel.go` file for the `pkg/otelx` package
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

---
+++ pkg/otelx/otel.go
@@ -0,0 +1,11 @@
+package otelx
+
+import (
+	"context"
+)
+
+// SetupOTelSDK sets up the OpenTelemetry SDK with the Jaeger exporter.
+func SetupOTelSDK() (shutdown func(context.Context) error, err error) {
+	// todo: 2024/6/8|sean|implement this function
+	panic("not implemented")
+}
